331417b97274c357f52efa03758f5d08465edded
Reuven Lerner <reuven@lerner.co.il>
2020-07-28 12:00:28 +0300

    gitautosync update 64

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ def menu(*args):
     while True:
         choices = '/'.join(args)
 
-        choice = raw_input("Enter a choice {0}: ".format(choices)
+        choice = raw_input("Enter a choice ({0}): ".format(choices))
 
         if choice in args:
             return choice
